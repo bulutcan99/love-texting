e920f2a81f370a35336e624fb11907cc15561c9b
bulutcan99 <gocerbulutcan@gmail.com>
2024-11-09 15:08:39 +0300

    feat

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,11 @@ pub mod ui;
 //TODO: commands olarak kendi komutlarimizi yazacagiz
 //daha moduler hale getirecegiz
 //ve not defteri formatina benzetecegiz.
+
+//TODO: state olarak dosya islemlerini yapacagiz
+//dosya acma, dosya kaydetme, dosya silme, dosya guncelleme
+
+//TODO: ui olarak islemleri ayirip builderda birlestirecegiz
 fn main() {
     let main_window = WindowDesc::new(ui_builder())
         .title(LocalizedString::new("open-save-demo").with_placeholder("Opening/Saving Demo"));
