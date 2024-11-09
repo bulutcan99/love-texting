297515eb9b996135e6443abcabcbef397284e41f
bulutcan99 <gocerbulutcan@gmail.com>
2024-11-09 15:06:38 +0300

    feat

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use druid::{AppLauncher, LocalizedString, WindowDesc};
+use ui::{ui_builder, Delegate};
+
 pub mod command;
 pub mod state;
 pub mod ui;
 fn main() {
-    println!("Hello, world!");
+    let main_window = WindowDesc::new(ui_builder())
+        .title(LocalizedString::new("open-save-demo").with_placeholder("Opening/Saving Demo"));
+    let data = "Type here.".to_owned();
+    AppLauncher::with_window(main_window)
+        .delegate(Delegate)
+        .log_to_console()
+        .launch(data)
+        .expect("launch failed");
 }
