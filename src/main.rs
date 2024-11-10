60627fa51e461bff01933f46c47c99803d79a3d8
bulutcan99 <gocerbulutcan@gmail.com>
2024-11-10 20:59:18 +0300

    started to texting

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,5 @@
-use druid::{AppLauncher, LocalizedString, WindowDesc};
-use ui::{ui_builder, Delegate, UiState};
+use anyhow::Error;
 
-pub mod command;
-pub mod state;
-pub mod ui;
-
-//TODO: commands olarak kendi komutlarimizi yazacagiz
-//daha moduler hale getirecegiz
-//ve not defteri formatina benzetecegiz.
-
-//TODO: state olarak dosya islemlerini yapacagiz
-//dosya acma, dosya kaydetme, dosya silme, dosya guncelleme
-
-//TODO: ui olarak islemleri ayirip builderda birlestirecegiz
-fn main() {
-    let main_window = WindowDesc::new(ui_builder())
-        .title(LocalizedString::new("open-save-demo").with_placeholder("Opening/Saving Demo"));
-
-    let data = UiState {
-        font_size: 14.0,
-        input_text: "Type here.".to_owned(),
-    };
-
-    AppLauncher::with_window(main_window)
-        .delegate(Delegate)
-        .log_to_console()
-        .launch(data)
-        .expect("launch failed");
+fn main() -> Result<(), Error> {
+    Ok(())
 }
