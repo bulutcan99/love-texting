0d3a743ee2bf7bde375239d8426b5bd370d143b2
bulutcan99 <gocerbulutcan@gmail.com>
2024-11-19 20:47:56 +0300

    feat

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,19 @@ use std::io::{self, Read};
 use anyhow::{Error, Ok};
 use crossterm::terminal;
 
+struct CleanUp;
+impl Drop for CleanUp {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().expect("Could not disable raw mode!");
+    }
+}
+
 fn main() -> Result<(), Error> {
+    let _ = CleanUp;
     terminal::enable_raw_mode()?;
-    let mut buf = Vec::new();
-    dbg!("Hello, World!");
+    let mut buf = Vec::with_capacity(1);
     buf.push(10);
     while io::stdin().read(&mut buf)? > 0 && buf != [b'q'] {}
+    panic!("User quit the program");
     Ok(())
 }
